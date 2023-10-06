3c77fc912cb5210420d43bab5e114e8a4e7d551f
karan1000000 <chauhankaran00080@gmail.com>
2023-10-06 21:16:47 +0530

    change

---
+++ src/container/students/Components/EamQuestions.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const EamQuestions = () => {
+  return (
+    <>
+    <div className="container">
+        <p>1.A train running at the speed of 60 km/hr crosses a pole in 9 seconds. What is the length of the train?
+            <br /> A . option 1 <br />B. Option 2 <br />c.Option 3 <br />D . option 4
+        </p>
+        <hr />
+    </div>
+
+      
+    </>
+  )
+}
+
+export default EamQuestions
