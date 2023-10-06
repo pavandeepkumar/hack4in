9d5685ffff2cff26551238c280cc4a8b07e279fa
Pavandeep <pavandeepkumarmlk@gmail.com>
2023-10-06 12:50:08 +0530

    routing setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
-import { useState } from 'react'
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
+import Login from './container/user/login/Login'
+import Students from './container/students/Students'
+import Registation from './container/user/register/Registation'
+import Teacher from './container/teacher/Teacher'
+import Admin from './container/admin/Admin'
+
 
 function App() {
-
-
   return (
     <>
-      <h1>Welcome to hack for india</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Students />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Registation />} />
+          <Route path='/teacher' element={<Teacher />} />
+          <Route path='/admin' element={<Admin />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
